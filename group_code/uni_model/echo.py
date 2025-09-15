fddb9aaea568c4b8f09b4963a36436e893875e3d
Kelly Ding <kellydingzx@jKX712D6JR.local>
2025-09-15 15:59:41 +0100

    a

--- group_code/uni_model/echo.py
+++ group_code/uni_model/echo.py
@@ -1,5 +1,7 @@
 from mmai25_hackathon.dataset import * 
 from mmai25_hackathon.load_data.echo import * 
+from torch.utils.data import Dataset
+
 
 class ECHO_uni(Dataset):
     def __init__(self, mod_root):
