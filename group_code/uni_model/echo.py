b06165fc96c27c4d1b15ae1380c89012531fbf09
Daniel Onah <28272361+DanFON@users.noreply.github.com>
2025-09-15 15:55:42 +0100

    echo.py

--- group_code/uni_model/echo.py
+++ group_code/uni_model/echo.py
@@ -1,1 +1,22 @@
+from mmai25_hackathon.dataset import * 
+from mmai25_hackathon.load_data.echo import * 
 
+class ECHO_uni(Dataset):
+    def __init__(self, mod_root):
+        self.records = load_mimic_iv_echo_record_list(mod_root)
+        
+    def __len__(self) -> int:
+        return len(self.records)
+    
+    def __getitem__(self, idx: int):
+        subject_id = self.records.iloc[idx]["subject_id"]
+        sig, fields = load_echo_record(self.records.iloc[idx]["echo_path"])
+
+        return {subject_id: [sig, fields]}
+    
+    def get_idx_by_subject(self, subject_id):
+        return self.records.index[self.records["subject_id"] == subject_id][0]
+
+    def fetch(self, subject_id):
+        idx = self.get_idx_by_subject(subject_id)
+        return self.__getitem__(idx)
